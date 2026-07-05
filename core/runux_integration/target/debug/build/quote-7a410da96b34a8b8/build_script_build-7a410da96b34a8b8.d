4b080e6b54489226790f7d9669069d5d4ab985f7
Xavier Callens <callensxavier@gmail.com>
2026-07-05 09:53:05 +0000

    test(core): Implement unit tests for Runux T4 engine mapping

---
+++ core/runux_integration/target/debug/build/quote-7a410da96b34a8b8/build_script_build-7a410da96b34a8b8.d
@@ -0,0 +1,5 @@
+/home/callensxavier_gmail_com/SocrateAI-Scientific-Agora-Home/core/runux_integration/target/debug/build/quote-7a410da96b34a8b8/build_script_build-7a410da96b34a8b8.d: /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.46/build.rs
+
+/home/callensxavier_gmail_com/SocrateAI-Scientific-Agora-Home/core/runux_integration/target/debug/build/quote-7a410da96b34a8b8/build_script_build-7a410da96b34a8b8: /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.46/build.rs
+
+/home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.46/build.rs:
---
+++ core/runux_integration/target/debug/deps/itoa-97012b5246acbeaf.d
@@ -0,0 +1,8 @@
+/home/callensxavier_gmail_com/SocrateAI-Scientific-Agora-Home/core/runux_integration/target/debug/deps/itoa-97012b5246acbeaf.d: /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs
+
+/home/callensxavier_gmail_com/SocrateAI-Scientific-Agora-Home/core/runux_integration/target/debug/deps/libitoa-97012b5246acbeaf.rlib: /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs
+
+/home/callensxavier_gmail_com/SocrateAI-Scientific-Agora-Home/core/runux_integration/target/debug/deps/libitoa-97012b5246acbeaf.rmeta: /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs /home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs
+
+/home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/lib.rs:
+/home/callensxavier_gmail_com/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.18/src/u128_ext.rs:
